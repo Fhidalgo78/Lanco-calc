7e29d74dceccfaf5f6895f0fbb410b3ff2402aae
Leonardo Villegas <66802519+LeoVB@users.noreply.github.com>
2023-11-08 16:49:50 -0600

    avance 2

--- JS/Calculadora.js
+++ JS/Calculadora.js
@@ -1,57 +1,175 @@
-import { divPuertasVentanas, nuevoDivParedes, divParedes, nuevoDivPuertasVentanas } from './constantes.js';
+import { divPuertasVentanas, divParedes } from "./constantes.js";
+
+let unidadMedidad = "m2";
+let ft = 10.7639;
+let inputParedesValidation = 0;
+let inputPuertasValidation = 0;
 
 $(document).ready(function () {
+  // Agregar un nuevo div con el contenido "Paredes" al hacer clic en el botón "Add1"
+  $("#addVentana-btn").click(agregarDivParedes);
 
+  // Agregar un nuevo div con el contenido "PUERTAS Y VENTANAS" al hacer clic en el botón "Add2"
+  $("#addPuerta-btn").click(agregarDivPuertasVentanas);
 
-    // Agregar un nuevo div con el contenido "Paredes" al hacer clic en el botón "Add1"
-    $("#addVentana-btn").click(agregarDivParedes);
+  $("#calcular-btn").click(buttonListener);
 
-    // Agregar un nuevo div con el contenido "PUERTAS Y VENTANAS" al hacer clic en el botón "Add2"
-    $("#addPuerta-btn").click(agregarDivPuertasVentanas);
+  // Agregar un nuevo div al hacer clic en el botón "Agregar Div"
+  //$("#agregar-div-btn").click(agregarDiv);
 
-    $("#calcular-btn").click(calcular);
+  // Borrar un div al hacer clic en el ícono trash
+  $(document).on("click", ".borrar-div", borrarDiv);
 
-    // Agregar un nuevo div al hacer clic en el botón "Agregar Div"
-    //$("#agregar-div-btn").click(agregarDiv);
+  //Cambia unidad de medida de metros a pies y viceversa con checkbox
+  $("#color_mode").on("change", function () {
+    changeColorMode(this);
+  });
 
-    // Borrar un div al hacer clic en el ícono trash
-    $(document).on("click", ".borrar-div", borrarDiv);
-
-
-
+  $("body").on("blur", ".small-input", function () {
+    verificaInputNumerico();
+  });
 });
 
 // Función para borrar un div al hacer clic en el ícono trash
 function borrarDiv() {
-    $(this).closest(".row-container-wrapper").remove();
+  $(this).closest(".row-container-wrapper").remove();
+  verificaInputNumerico();
 }
 // Función para agregar un nuevo div
 function agregarDivParedes() {
+  $("#contenedor-divs-paredes").append(divParedes);
 
-    $("#contenedor-divs-paredes").append(divParedes);
-
-
-    //$("#contenedor-divs").append(nuevoDiv);
+  //$("#contenedor-divs").append(nuevoDiv);
 }
 
 // Función para agregar un nuevo div con el contenido "PUERTAS Y VENTANAS"
 function agregarDivPuertasVentanas() {
-    const nuevoDiv =
-        $("#contenedor-divs-puertas").append(divPuertasVentanas);
+  const nuevoDiv = $("#contenedor-divs-puertas").append(divPuertasVentanas);
 }
 
+function buttonListener() {
+  verificaInputNumerico();
 
-function calcular() {
+  if (inputParedesValidation >= 2 && inputParedesValidation % 2 == 0) {
     // Usando jQuery para seleccionar elementos por clase
-    const valoresParedes = $('.paredes-input').map(function () {
-        return $(this).val();
-    }).get();
+    const valoresParedesLargos = $(".paredes-input-largo")
+      .map(function () {
+        return parseFloat($(this).val());
+      })
+      .get();
 
-    const valoresPuertas = $('.puertas-input').map(function () {
-        return $(this).val();
-    }).get();
+    const valoresParedesAnchos = $(".paredes-input-ancho")
+      .map(function () {
+        return parseFloat($(this).val());
+      })
+      .get();
+
+    const valoresPuertasLargos = $(".puertas-input-largo")
+      .map(function () {
+        return parseFloat($(this).val());
+      })
+      .get();
+
+    const valoresPuertasAnchos = $(".puertas-input-ancho")
+      .map(function () {
+        return parseFloat($(this).val());
+      })
+      .get();
 
     // La variable "valores" contendrá un array con los valores de los inputs
-    console.log(valoresParedes);
-    console.log(valoresPuertas);
+    calcularTotalArea(
+      valoresParedesAnchos,
+      valoresParedesLargos,
+      valoresPuertasAnchos,
+      valoresPuertasLargos
+    );
+  } else {
+    alert("Debe ingresar al menos dos valores");
+  }
+
+  if(!inputPuertasValidation ){
+    alert("suwi");
+  }
+
+}
+function changeColorMode(ele) {
+  unidadMedidad = $(ele).prop("checked") ? "ft" : "m2";
+}
+
+function calcularTotalArea(
+  valoresParedesAnchos,
+  valoresParedesLargos,
+  valoresPuertasAnchos,
+  valoresPuertasLargos
+) {
+  let totalAreaParedes = 0;
+  let totalAreaPuertas = 0;
+  let totalArea = 0;
+  let resultado = 0;
+  let galonesTotales = 0;
+
+  //se multiplica el ancho por el largo de cada pared y se suma
+
+  for (let i = 0; i < valoresParedesAnchos.length; i++) {
+    totalAreaParedes += valoresParedesAnchos[i] * valoresParedesLargos[i];
+  }
+
+  for (let i = 0; i < valoresPuertasAnchos.length; i++) {
+    totalAreaPuertas += valoresPuertasAnchos[i] * valoresPuertasLargos[i];
+  }
+
+  //el total de area de las paredes se resta con el total de area de las puertas y ventanas
+  resultado = totalAreaParedes - totalAreaPuertas;
+
+  console.log(unidadMedidad);
+
+  //segun la unidad de medida se hace la conversion
+
+  if (unidadMedidad === "m2") {
+    totalArea = resultado;
+    galonesTotales = totalArea / 15;
+  } else {
+    totalArea = resultado * ft;
+    galonesTotales = resultado / ft / 15;
+  }
+
+  console.log(galonesTotales);
+  console.log(totalArea);
+
+  return totalArea;
+}
+
+function verificaInputNumerico() {
+  // Recupera los divs con la clase "div-generado"
+  var $divsGenerados = $(".small-input");
+
+  // Itera a través de los divs para recuperar los valores
+  inputParedesValidation = 0;
+  for (let i = 0; i < $divsGenerados.length; i++) {
+    //verifica si no es numero
+    if (isNaN($divsGenerados[i].value)) {
+      alert("Ingrese solo numero");
+    }
+    //verifica si es un input de paredes y no esta vacio
+    if (
+      $divsGenerados[i].classList.contains("paredes-input") &&
+      $divsGenerados[i].value != ""
+    ) {
+      inputParedesValidation++;
+      //verifica si es un input de puertas y ventanas y esta vacio
+      // } else if (
+      //   $divsGenerados[i].classList.contains("paredes-input") &&
+      //   $divsGenerados[i].value === ""
+      // ) {
+      //   inputParedesValidation--;
+      // }
+    }
+    if (
+      $divsGenerados[i].classList.contains("puertas-input") &&
+      $("#contenedor-divs-puertas").children().length > 0 &&
+      $divsGenerados[i].value === ""
+    ) {
+        inputPuertasValidation = false;
+    }
+  }
 }
